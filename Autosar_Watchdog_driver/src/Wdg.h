aa33792bfd2ffab6ce97058e128b8c8170750cd9
hossammagdy2008 <hossammagdy2008@gmail.com>
2021-04-22 15:49:04 +0200

    add and fix the mode  and add a little code in wdg.c

--- Autosar_Watchdog_driver/src/Wdg.h
+++ Autosar_Watchdog_driver/src/Wdg.h
@@ -11,9 +11,16 @@
 #include "Wdglf_Types.h"
 #include "Wdg_Cfg.h"
 
-
+/*
 #define     WDGIF_OFF_MODE      1
 #define     WDGIF_SLOW_MODE     2
 #define     WDGIF_FAST_MODE     3
+*/
+
+typedef enum{
+WDGIF_OFF_MODE,
+WDGIF_SLOW_MODE,
+WDGIF_FAST_MODE
+}WdgIf_ModeType;
 
 #endif /* WDG_H_ */
--- Autosar_Watchdog_driver/src/Wdg_Cfg.h
+++ Autosar_Watchdog_driver/src/Wdg_Cfg.h
@@ -9,5 +9,13 @@
 #define WDG_CFG_H_
 
 
+#define     WD_MODE_IS_AVAILABE         1
+#define     WD_MODE_IS_NOT_AVAILABE     2
+
+#define     WD_OFF_MODE       WD_MODE_IS_AVAILABE
+#define     WD_SLOW_MODE      WD_MODE_IS_AVAILABE
+#define     WD_FAST_MODE      WD_MODE_IS_AVAILABE
+
+
 
 #endif /* WDG_CFG_H_ */
